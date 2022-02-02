f497d521022b345e56095d864a897e5b6792ac12
Pedro Hinojosa <petehinojosa85@gmail.com>
2022-02-01 19:24:31 -0600

    first commit

---
+++ strings.py
@@ -0,0 +1,34 @@
+"""
+while True:
+    print('Enter your age:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a number for your age.')
+
+while True:
+    print('Select a new password (letters and numbers only):')
+    password = input()
+    if password.isalnum():
+        break
+    print('Passwords can only have letters and numbers.')
+"""
+
+s = " , "
+
+s = s.join(["test", "this", "out"])
+
+print(s)
+
+s = s.center(40,"=")
+
+print(s)
+
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 5)
+printPicnic(picnicItems, 20, 6)
